6a8711427af71d2d35a3986ad9aade596c2722b1
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-07-06 11:47:44 +0500

    Updaet main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print('123')
-bdsjfblksfb;kas
+# bu dastur ekranga so'z chiqaradi
+# x = int(input())
+# if x > 10:
+#     print("X 10 dan katta")
+# else:
+#     print("X 10 dan kichik")
+
+# x ni krit
+# agar x 10 dan kichik bolsa 
+#     x 10 dan kichik 
+# aks holda
+#     x 10 dan katta 
+
+# Interpretator - kodni sintaksis xatolarni tekshiruvchi dastur   
+# Compilator - kodni mashina kodiga o'tkazuvchi dastur
+
+# Python - Interpetatsiyadan keyin ishlaydi
+# C , C++ to'g'ridan to'g'ri kompilyatsiya qiladi 
+# IDE - Integrated development environment 
+# Integral dasturlash muhiti 
+# venv - Virtual environment 
