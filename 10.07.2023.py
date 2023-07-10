3f8ef85649898540081e859130b4763ab8194f16
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-07-10 12:56:37 +0500

    create 10.07.2023

---
+++ 10.07.2023.py
@@ -0,0 +1,112 @@
+# 16
+# x1=int(input('x1..'))
+# # a=input()
+# x2=int(input('x2..'))
+# print(abs(x1-x2))
+
+
+# 17
+# a=int(input('a'))
+# b=int(input('b'))
+# c=int(input('c'))
+# print(abs(c-a))
+# print(abs(c-b))
+# print(abs(c-a)+abs(c-b))
+
+# 19
+# from math import sqrt
+# # a
+# x1=int(input('x1..'))
+# y1=int(input('y1..'))
+# # b
+# x2=int(input('x2..'))
+# y2=int(input('y2..'))
+# # c
+# x3=int(input('x3..'))
+# y3=int(input('y3..'))
+
+# ab=((x2-x1)**2+(y2-y1)**2)**0.5
+# bc=((x2-x3)**2+(y2-y3)**2)**0.5
+# ca=((x3-x1)**2+(y3-y1)**2)**0.5
+
+# p=(ab+bc+ca)/2
+# a,b,c=c,b,a
+
+
+# a=abs(x1-x2)
+# b=abs(y1-y2)
+
+# p=2*(a+b)
+# s=a*b
+
+# print(p)
+# print(s)
+
+# 29
+# a=int(input('>>>'))
+# r=(3.14/180)*a
+# 35
+# v=int(input('>>>'))
+# u=int(input('>>>'))
+
+# t1=int(input('>>>'))
+# t2=int(input('>>>'))
+
+# s1=(v+u)*t1
+# s2=(v-u)*t2
+
+# # 36
+# v1=int(input('>>>'))
+# v2=int(input('>>>'))
+# t=int(input('>>>'))
+# s=int(input('>>>'))
+
+# (v1+v2)*t+s
+# s-(v1+v2)*t
+# s/(v1+v2)
+
+# d=b*b-4*a*c
+# x1=(-b+d**0.5)/(2*a)
+# x2=(-b-d**0.5)/(2*a)
+
+
+# l=int(input())
+# print(l/100)
+
+
+
+
+
+
+
+
+# a=input('bajkfbkjafbka')
+    
+# print(bool(a))
+
+
+# print(2+3)
+# print(5%2)
+# print(5//2)
+# print(5**10)
+# a=a+b
+# a+=b
+
+# a=a-b
+# a-=b
+# a*=b
+# a/=b
+# a**=b
+
+# a=5
+# a==5
+# print(bool(5==6))
+# print(bool(5!=6))
+# print(bool(5>6))
+# print(bool(5<6))
+# print(bool(6<=6))
+# print(bool(6>=6))
+
+print(bool(not(5>5)))
+
+(6>7) and (9>1) and not(5>5)
