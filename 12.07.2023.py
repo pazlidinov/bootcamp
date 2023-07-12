53c3b7f00e78095c40510df502c99d757096c627
pazlidinov <97672036+pazlidinov@users.noreply.github.com>
2023-07-12 11:55:21 +0500

    create list and other file

---
+++ 12.07.2023.py
@@ -0,0 +1,33 @@
+# # 14
+# n=int(input('>>>'))
+# # s=n[2]+n[0]+n[1]
+# s1=n//100
+# s2=(n//10)%10
+# s3=n%10
+
+# print(s1)
+# print(s2)
+# print(s3)
+
+
+# a=int(input('a..'))
+# b=int(input('b..'))
+# c=int(input('c..'))
+# n=(a//c)*(b//c)
+# s=(a*b)-n*c*c
+# print(s)
+
+# n=int(input('n..'))
+# print((int(input('>>>'))%1000)//100)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ list.py
@@ -0,0 +1,62 @@
+# lists
+a=[12, 'hdkf', True, False]
+
+# print(a[0])
+# a[0]=75
+# a[2]='hfjksdf'
+# print(a)
+# print(type(a))
+
+# print(a[-1]=='0000')
+# print(a[1:3])
+# print(a[-3:-1])
+# print(a[::1])
+# a[1:]=[45,65]
+# a.insert(1, 45)
+
+# a.append('bdjf')
+b=[45,'4857']
+
+# a[2]=b
+# a.append(b)
+# a.extend(b)
+# print(a)
+
+
+
+# a=[12, 'hdkf', True, False, 12]
+
+# a.remove(12)
+
+# a.pop()
+
+# del a[0]
+# del a
+# a.clear()
+# print(a)
+
+
+
+
+a=[12, 56,89,6,10,12, -1]
+
+
+
+
+# a.sort()
+# a.sort(reverse=True)
+
+# # a.reverse()
+# d=[5]
+# b=a
+# b[0]=0
+# c=a.copy()
+# d=list(a)
+# # print(d)
+# print(len(a))
+# print(a.count(12))
+# print(a.index(56))
+
+
+for i in range(7):
+    print(a[i])
